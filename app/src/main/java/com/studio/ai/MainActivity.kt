cf74ed317cff049206066fe085daa4f816d972f7
a647273991 <a647273991@gmail.com>
2026-06-01 19:59:20 +0800

    fix: simplify to plain WebView

--- app/src/main/java/com/studio/ai/MainActivity.kt
+++ app/src/main/java/com/studio/ai/MainActivity.kt
@@ -5,58 +5,28 @@ import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     private lateinit var webView: WebView
-    private lateinit var tabLayout: TabLayout
-
-    private val aiUrl = "http://127.0.0.1:5000"
-    private val nsfwUrl = "http://127.0.0.1:5001"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         webView = findViewById(R.id.webView)
-        tabLayout = findViewById(R.id.tabLayout)
-
-        setupWebView()
-        webView.loadUrl(aiUrl)
-
-        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab) {
-                when (tab.position) {
-                    0 -> webView.loadUrl(aiUrl)
-                    1 -> webView.loadUrl(nsfwUrl)
-                }
-            }
-            override fun onTabUnselected(tab: TabLayout.Tab) {}
-            override fun onTabReselected(tab: TabLayout.Tab) {}
-        })
-    }
-
-    private fun setupWebView() {
         webView.settings.apply {
             javaScriptEnabled = true
             domStorageEnabled = true
             mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
             allowFileAccess = true
-            cacheMode = WebSettings.LOAD_DEFAULT
             useWideViewPort = true
             loadWithOverviewMode = true
-            setSupportZoom(true)
-            builtInZoomControls = true
-            displayZoomControls = false
         }
         webView.webViewClient = WebViewClient()
+        webView.loadUrl("http://127.0.0.1:5000")
     }
 
     override fun onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack()
-        } else {
-            super.onBackPressed()
-        }
+        if (webView.canGoBack()) webView.goBack() else super.onBackPressed()
     }
 }
